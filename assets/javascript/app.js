60dfc204b8bcd6b5240a35a96d23f0604e92adf7
Roy Hinojosa <roy.hinojosa@gmail.com>
2016-06-03 00:11:25 -0600

    almost done with all the app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+	//======================================================================
+	//Variables for app
+	var gifArray = [];
+
+	//======================================================================
+	//functions for app
+	function renderButtons() {
+		//empty buttons or they will keep appending to the page
+		$('#displayGifButtons').empty();
+
+		for (var i = 0; i < gifArray.length; i++) {
+			var gifButton = $('<button>');
+			gifButton.addClass("gifs");
+			gifButton.attr('data-name', gifArray[i]);
+			gifButton.text(gifArray[i]);
+			$('#displayGifButtons').append(gifButton);
+		}
+	}
+
+	$('#addGif').on('click', function() {
+
+		var gif = $('#gif-input').val().trim();
+		gifArray.push(gif);
+
+		renderButtons();
+
+		return false;
+	});
+
+	function displayGifs() {
+		$('#displayGifs').empty();
+		var showGifs = $(this).attr("data-name");
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + showGifs + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+		$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
+			for (var i = 0; i < 10; i++) {
+				var gifImage = $('<img>');
+				gifImage.attr('src', response.data[i].images.original_still.url);
+				gifImage.attr('data-still', response.data[i].images.original_still.url);
+				gifImage.attr('data-animate', response.data[i].images.original.url);
+				gifImage.attr('data-state', "still");
+				gifImage.addClass('actualGif');
+				$('#displayGifs').append(gifImage);
+			}
+			startAndStopGifs();
+		});
+
+	}
+
+	function startAndStopGifs() {
+		$('.actualGif').on('click', function() {
+			var state = $(this).attr('data-state');
+
+			if (state === "still") {
+				$(this).attr('src', $(this).data('animate'));
+				$(this).attr('data-state', 'animate');
+			} else {
+				$(this).attr('src', $(this).data('still'));
+				$(this).attr('data-state', 'still');
+			}
+		});
+	}
+
+
+	$(document).on('click', '.gifs', displayGifs);
+
+});
+
+
+
+
